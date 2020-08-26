634ea0690c7dbc5004a449bc14b3e4093b1cdd78
itismeabdul <itismeabdul@gmail.com>
2020-08-26 08:21:10 +0100

    third class

--- authentication/url.py
+++ authentication/url.py
@@ -3,5 +3,6 @@ from authentication import views
 
 urlpatterns = [
 
-	path('login/', views.login, name = 'login')
+	path('login/', views.login, name = 'login'),
+	path('signup/', views.signup, name='signup')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -7,6 +7,21 @@ def login(request):
 	if request.method == 'GET':
 		return render(request, 'login.html')
 	
+	elif request.method == 'POST':
+		#handle login here
+		return HttpResponseRedirect('/')
+		
 	else:
 		return HttpResponseRedirect('/')
+	
+def signup(request):
+		if request.method=='GET':
+			return render(request,'signup.html')
+		
+		elif request.method =='POST':
+			#handle sign up here
+			return HttpResponseRedirect('/')
+		
+		else:
+			return HttpResponseRedirect('/')
 		
